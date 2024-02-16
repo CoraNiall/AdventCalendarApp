50ef66b43097679ac2eec57d66c7c8bd3ef0502f
Caroline Reiss <38852967+CoraNiall@users.noreply.github.com>
2024-02-16 14:06:23 +0000

    ready to start from here

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import styles from './page.module.css';
+import styles from './styles/globals.css';
 
 export default function Home() {
   return <main className={styles.body}></main>;
--- src/app/ui/calendar.js
+++ src/app/ui/calendar.js
@@ -2,21 +2,21 @@ import styles from '@/app/styles/calendar.module.css';
 
 export default function Calendar() {
   return (
-    <div class={styles.calendarContainer}>
-      <header class={styles.calendarHeader}>
-        <p class={styles.calendarCurrentDate}></p>
-        <div class={styles.calendarNavigation}>
-          <span id="calendar-prev" class={styles.materialSymbolsRounded}>
-            chevron_left
+    <div className={styles.calendarContainer}>
+      <header className={styles.calendarHeader}>
+        <p className={styles.calendarCurrentDate}></p>
+        <div className={styles.calendarNavigation}>
+          <span id="calendar-prev" className={styles.materialSymbolsRounded}>
+            {'<'}
           </span>
-          <span id="calendar-next" class={styles.materialSymbolsRounded}>
-            chevron_right
+          <span id="calendar-next" className={styles.materialSymbolsRounded}>
+            {'>'}
           </span>
         </div>
       </header>
 
-      <div class={styles.calendarBody}>
-        <ul class={styles.calendarWeekdays}>
+      <div className={styles.calendarBody}>
+        <ul className={styles.calendarWeekdays}>
           <li>Sun</li>
           <li>Mon</li>
           <li>Tue</li>
@@ -25,7 +25,7 @@ export default function Calendar() {
           <li>Fri</li>
           <li>Sat</li>
         </ul>
-        <ul class={styles.calendarDates}></ul>
+        <ul className={styles.calendarDates}></ul>
       </div>
     </div>
   );
