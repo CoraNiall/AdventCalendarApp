9c7ed7f6e62e3188e3f490385084b5bbac5e51fd
Caroline Reiss <38852967+CoraNiall@users.noreply.github.com>
2024-09-01 14:13:27 +0100

    turning on click event

--- src/app/ui/advent-window.jsx
+++ src/app/ui/advent-window.jsx
@@ -1,17 +1,40 @@
+'use client';
+import { useState } from 'react';
 import Image from 'next/image';
 import styles from '@/app/styles/advent-window.module.css';
 
 export default function AdventWindow({ image }) {
+  const [flip, setFlip] = useState(false);
+
   return (
     <section id={image.id} className={styles.adventWindowSection}>
       <div className={styles.adventWindowCard}>
-        <div className={styles.adventWindowInner}>
-          <div className={styles.adventWindowFront}>
-            <h2>{image.id}</h2>
+        <div
+          className={`${
+            flip
+              ? `${styles.adventWindowInnerFlip}`
+              : `${styles.adventWindowInner}`
+          }`}
+        >
+          <div
+            className={styles.adventWindowFront}
+            key={image.id}
+            onClick={() => {
+              setFlip(!flip);
+            }}
+          >
+            <h2 className={styles.adventWindowCardHeader}>{image.id}</h2>
           </div>
-          <div className={styles.adventWindowBack}>
+          <div
+            className={styles.adventWindowBack}
+            key={image.src}
+            onClick={() => {
+              setFlip(!flip);
+            }}
+          >
             <Image
               className={styles.adventWindowImg}
+              key={image.id}
               src={image.src}
               width={image.width}
               height={image.height}
